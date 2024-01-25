e41d1db8bb5f6a8fee78c24a2bc96fc56510856d
root <root@AuliaFNC15>
2024-01-25 17:08:50 +0700

    first commit

---
+++ app/Filament/Resources/ProductResource/Widgets/TotalProduct.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Filament\Resources\ProductResource\Widgets;
+
+use App\Models\Product;
+use App\Models\User;
+use App\Models\Tag;
+use App\Models\Category;
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use Filament\Widgets\StatsOverviewWidget\Stat;
+
+
+class TotalProduct extends BaseWidget
+{
+    protected function getStats(): array
+    {
+        $reserve = Product::all()->count();
+        $user = User::all()->count();
+        $tagging = Tag::all()->count();
+        $cat = Category::all()->count();
+        return [
+            Stat::make(' ', $reserve)
+                ->description('Total Product')
+                ->descriptionIcon('heroicon-o-building-storefront')
+                ->color('success'),
+            Stat::make('jumlah user', $user),
+            Stat::make('jumlah tags', $tagging),
+            Stat::make('jumlah category', $cat),
+        ];
+    }
+}
